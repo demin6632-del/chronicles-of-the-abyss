3085a938f25c4724785e40ea748e303c6c12df8e
demin6632-del <demin6632@gmail.com>
2026-09-05 00:19:00 +0500

    Добавить Android Activity

---
+++ app/src/main/java/com/chroniclesoftheabyss/game/MainActivity.java
@@ -0,0 +1,22 @@
+package com.chroniclesoftheabyss.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends Activity {
+    @Override public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = new WebView(this);
+        WebSettings s = webView.getSettings();
+        s.setJavaScriptEnabled(true);
+        s.setDomStorageEnabled(true);
+        s.setAllowFileAccess(true);
+        s.setAllowContentAccess(true);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("file:///android_asset/game/game.html");
+        setContentView(webView);
+    }
+}
